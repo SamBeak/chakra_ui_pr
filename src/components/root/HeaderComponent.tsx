fd13d304c9c56766bbbe68e26c81f0e5f123d165
SamBeak <114740158+SamBeak@users.noreply.github.com>
2024-07-31 17:37:14 +0900

    chore: Update project configuration files
    Update tsconfig.json, tsconfig.paths.json, and craco.config.js to improve project configuration and maintainability.

--- src/components/root/HeaderComponent.tsx
+++ src/components/root/HeaderComponent.tsx
@@ -1,14 +1,29 @@
-import { Box, GridItem, Heading, HStack } from "@chakra-ui/react";
 import React from "react";
+import {
+  Box,
+  Flex,
+  GridItem,
+  Link as ChakraLink,
+  Skeleton,
+  Image,
+} from "@chakra-ui/react";
+import { Link as router } from "react-router-dom";
+import mainLogo from "@assets/images/main_logo.png";
 
 export default function HeaderComponent() {
   return (
     <GridItem gridArea={"header"}>
-      <Box>
-        <HStack>
-          <Heading>Header</Heading>
-        </HStack>
-      </Box>
+      <Flex>
+        <Box>
+          <Skeleton isLoaded={true}>
+            <ChakraLink as={router} to="/">
+              <Image src={mainLogo} alt="logo" />
+            </ChakraLink>
+          </Skeleton>
+        </Box>
+        <Box></Box>
+        <Box></Box>
+      </Flex>
     </GridItem>
   );
 }
