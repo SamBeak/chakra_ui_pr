26ef3fb5af0c30ff3ace87faf71e73c4e1d69718
SamBeak <114740158+SamBeak@users.noreply.github.com>
2024-07-31 10:44:33 +0900

    chore: Add breakpoints to theme configuration

--- src/theme.ts
+++ src/theme.ts
@@ -1,10 +1,11 @@
 import { extendTheme, ThemeConfig } from "@chakra-ui/react";
+import breakpoints from "./breakpoints";
 
 const config: ThemeConfig = {
-    initialColorMode: "light",
-    useSystemColorMode: false,
+  initialColorMode: "light",
+  useSystemColorMode: false,
 };
 
-const theme = extendTheme({ config });
+const theme = extendTheme({ config, breakpoints });
 
 export default theme;
