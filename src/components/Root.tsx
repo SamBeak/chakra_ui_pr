ce3756906102bc2e290be401ece7df4373c01303
SamBeak <114740158+SamBeak@users.noreply.github.com>
2024-07-31 10:57:39 +0900

    chore: Update Root component grid layout
     Update the grid layout in the Root component to make it responsive and improve code readability and maintainability.

--- src/components/Root.tsx
+++ src/components/Root.tsx
@@ -6,21 +6,42 @@ import FooterComponent from "./root/FooterComponent";
 import NavComponent from "./root/NavComponent";
 
 export default function Root() {
-    return (
-        <Grid
-            gridTemplateAreas={`"header header"
-                                "nav main"
-                                "footer footer"`}
-            gridTemplateColumns={`20rem 1fr`}
-            gridTemplateRows={`15rem 1fr 10rem`}
-            h="100vh"
-            w="100vw"
-            p="1rem"
-        >
-            <HeaderComponent />
-            <NavComponent />
-            <Outlet />
-            <FooterComponent />
-        </Grid>
-    );
+  return (
+    <Grid
+      gridTemplateAreas={{
+        base: `"header" "nav" "main" "footer"`,
+        md: `"header" "nav" "main" "footer"`,
+        lg: `"header header"
+             "nav main"
+             "footer footer"`,
+        xl: `"header header"
+             "nav main"
+             "footer footer"`,
+        "2xl": `"header header"
+                "nav main"
+                "footer footer"`,
+      }}
+      gridTemplateColumns={{
+        base: `1fr`,
+        md: `1fr`,
+        lg: `15rem 1fr`,
+        xl: `20rem 1fr`,
+        "2xl": `20rem 1fr`,
+      }}
+      gridTemplateRows={{
+        base: `auto auto 1fr auto`,
+        md: `auto auto 1fr auto`,
+        lg: `10rem 1fr 5rem`,
+        xl: `15rem 1fr 10rem`,
+      }}
+      h="100vh"
+      w="100vw"
+      p="1rem"
+    >
+      <HeaderComponent />
+      <NavComponent />
+      <Outlet />
+      <FooterComponent />
+    </Grid>
+  );
 }
